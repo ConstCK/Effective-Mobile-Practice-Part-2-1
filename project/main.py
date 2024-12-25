d29438b97705410088b9704d1a1477c0e9fbef31
Constantin <Constantin_CK@outlook.com>
2024-12-25 17:36:09 +0300

    Change some text

--- project/main.py
+++ project/main.py
@@ -12,4 +12,4 @@ if __name__ == '__main__':
     try:
         asyncio.run(main())
     except Exception:
-        print('exiting program...')
+        print('There are some problems running application...exiting program...')
